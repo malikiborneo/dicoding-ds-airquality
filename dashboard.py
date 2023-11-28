4db742ad5f2cfbb67b621272688bbcf370dfe2d8
Reza Maliki Akbar <rezamaliki.akbar@gmail.com>
2023-11-28 11:48:02 +0000

    Write subheader conclusion

--- dashboard.py
+++ dashboard.py
@@ -53,3 +53,72 @@ plt.title('Average Monthly PM2.5 Levels')
 plt.xlabel('Month')
 plt.ylabel('Average PM2.5')
 st.pyplot(fig)
+
+To create a detailed and comprehensive Streamlit dashboard that aligns with your assignment requirements and aims for a 5-star rating, we should focus on making the dashboard interactive, insightful, and user-friendly, while avoiding machine learning techniques as per the assignment's guidelines. Here's an expanded and detailed guide for your dashboard:
+Expanded Streamlit Dashboard Script
+
+python
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load your dataset
+data = pd.read_csv('path_to_your_dataset.csv')
+
+# Title of the dashboard
+st.title('Air Quality Analysis Dashboard: Wanshouxigong Station')
+
+# Description
+st.write('This dashboard provides an interactive way to explore air quality data, specifically focusing on PM2.5 levels and their relationship with various weather conditions.')
+
+# Adding a sidebar for interactive inputs
+st.sidebar.header('User Input Features')
+
+# Let users select a year and month to view data
+selected_year = st.sidebar.selectbox('Select Year', list(data['year'].unique()))
+selected_month = st.sidebar.selectbox('Select Month', list(data['month'].unique()))
+
+# Filter data based on the selected year and month
+data_filtered = data[(data['year'] == selected_year) & (data['month'] == selected_month)]
+
+# Displaying data statistics
+st.subheader('Data Overview for Selected Period')
+st.write(data_filtered.describe())
+
+# Line chart for PM2.5 levels over selected month
+st.subheader('Daily PM2.5 Levels')
+fig, ax = plt.subplots()
+ax.plot(data_filtered['day'], data_filtered['PM2.5'])
+plt.xlabel('Day of the Month')
+plt.ylabel('PM2.5 Concentration')
+st.pyplot(fig)
+
+# Correlation heatmap for the selected month
+st.subheader('Correlation Heatmap of Air Quality Indicators')
+corr = data_filtered[['PM2.5', 'NO2', 'SO2', 'CO', 'O3', 'TEMP', 'PRES', 'DEWP']].corr()
+fig, ax = plt.subplots()
+sns.heatmap(corr, annot=True, ax=ax)
+plt.title('Correlation Heatmap')
+st.pyplot(fig)
+
+# Seasonal Trend Analysis
+st.subheader('Seasonal Trend Analysis')
+seasonal_trends = data.groupby('month')['PM2.5'].mean()
+fig, ax = plt.subplots()
+seasonal_trends.plot(kind='bar', color='skyblue', ax=ax)
+plt.title('Average Monthly PM2.5 Levels')
+plt.xlabel('Month')
+plt.ylabel('Average PM2.5')
+st.pyplot(fig)
+
+
+# Conclusion
+st.subheader('Conclusion')
+st.write("""
+- The dashboard allows for an in-depth analysis of air quality, focusing on PM2.5 levels.
+- Users can interactively select different time periods to observe changes and trends.
+- The heatmap provides insights into the relationships between different pollutants and weather conditions.
+- Seasonal trend analysis indicates significant variations in PM2.5 levels across different months.
+""")
