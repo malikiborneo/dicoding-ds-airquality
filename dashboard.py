9e620334a0f908d6278272c88b7d1ad52dd24725
Reza Maliki Akbar <rezamaliki.akbar@gmail.com>
2023-11-28 11:37:55 +0000

    Initial code, library, set path csv

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,11 @@
 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load your dataset
+data = pd.read_csv('PRSA_Data_Wanshouxigong_20130301-20170228.csv')
+
+# Title of the dashboard
+st.title('Air Quality Analysis Dashboard: Wanshouxigong Station')
